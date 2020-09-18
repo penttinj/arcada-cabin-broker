6006d3296aba3b0717c1a2f73a078f9658fcb879
Johan Penttinen <johanlepenttinen@gmail.com>
2020-09-18 21:00:17 +0300

    Change name services to components

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import {
   applyRoutes, logger,
 } from "./utils";
 import middleware from "./middleware";
-import apiRoutes from "./services";
+import apiRoutes from "./components";
 import errorHandlers from "./middleware/errorHandlers";
 import { initMongoDB } from "./config/mongodb";
 
